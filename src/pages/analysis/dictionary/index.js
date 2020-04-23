1468778eedb1172a3bedc9f92d616ea28461d4ba
joonwoo8888 <dev.joonwoo@gmail.com>
2020-04-23 14:26:41 +0900

    메뉴 UI 수정

--- src/pages/analysis/dictionary/index.js
+++ src/pages/analysis/dictionary/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import styled from "styled-components";
 import { NavLink as RouterNavLink } from "react-router-dom";
 
@@ -11,7 +13,12 @@ import {
   Breadcrumbs as MuiBreadcrumbs,
   Card as MuiCard,
   Divider as MuiDivider,
-  Typography
+  Typography,
+  AppBar,
+  Tabs,
+  Tab,
+  Box,
+  Paper
 } from "@material-ui/core";
 
 import { spacing } from "@material-ui/system";
@@ -26,22 +33,54 @@ const Divider = styled(MuiDivider)(spacing);
 
 const Breadcrumbs = styled(MuiBreadcrumbs)(spacing);
 
-function EmptyCard() {
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
   return (
-    <Card mb={6}>
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          Empty card
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Empty card
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  )
 }
 
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 function Blank() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <React.Fragment>
       <Helmet title="Blank" />
@@ -63,7 +102,53 @@ function Blank() {
 
       <Grid container spacing={6}>
         <Grid item xs={12}>
-          <EmptyCard />
+          <div className={classes.root}>
+
+          
+        <AppBar position="static" color="default">
+          <Tabs value={value} 
+                    onChange={handleChange} 
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="scrollable"
+                    scrollButtons="on"
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="scrollable force tabs example"
+              >
+                <Tab label="Item 0" {...a11yProps(0)} />
+                <Tab label="Item 1" {...a11yProps(1)} />
+                <Tab label="Item 2" {...a11yProps(2)} />
+                <Tab label="Item 3" {...a11yProps(3)} />
+                <Tab label="Item 4" {...a11yProps(4)} />
+                <Tab label="Item 5" {...a11yProps(5)} />
+                <Tab label="Item 6" {...a11yProps(6)} />
+                <Tab label="Item 7" {...a11yProps(7)} />
+                <Tab label="Item 8" {...a11yProps(8)} />
+                <Tab label="Item 9" {...a11yProps(9)} />
+                <Tab label="Item 10" {...a11yProps(10)} />
+                <Tab label="Item 11" {...a11yProps(11)} />
+                <Tab label="Item 12" {...a11yProps(12)} />
+              </Tabs></AppBar>
+            {/* <AppBar position="static">
+              
+            </AppBar> */}
+            <Divider />
+            <TabPanel value={value} index={0}>Item 1</TabPanel>
+            <TabPanel value={value} index={1}>Item 2</TabPanel>
+            <TabPanel value={value} index={2}>Item 3</TabPanel>
+            <TabPanel value={value} index={3}>Item 4</TabPanel>
+            <TabPanel value={value} index={4}>Item 5</TabPanel>
+            <TabPanel value={value} index={5}>Item 6</TabPanel>
+            <TabPanel value={value} index={6}>Item 7</TabPanel>
+            <TabPanel value={value} index={7}>Item 8</TabPanel>
+            <TabPanel value={value} index={8}>Item 9</TabPanel>
+            <TabPanel value={value} index={9}>Item 10</TabPanel>
+            <TabPanel value={value} index={10}>Item 11</TabPanel>
+            <TabPanel value={value} index={11}>Item 12</TabPanel>
+            <TabPanel value={value} index={12}>Item 13</TabPanel>
+            <TabPanel value={value} index={13}>Item 14</TabPanel>
+            </div>
         </Grid>
       </Grid>
     </React.Fragment>
--- src/routes/index.js
+++ src/routes/index.js
@@ -94,125 +94,141 @@ const authRoutes = {
   ]
 };
 
-const analysisRoutes = {
-  id: "분석",
-  path: "analysis",
-  icon: <Icon.Visibility />,
-  children: [
-    {
-      path: "/analysis/dictionary",
-      name: "사전",
-      component: Dictionary
-    },
-    {
-      path: "/analysis/tools",
-      name: "분석도구",
-      component: Tools
-    },
-    {
-      path: "/analysis/ranking-tuning",
-      name: "랭킹튜닝",
-      component: RankingTuning
-    }
-  ]
-}
+const analysisRoutes = [
+  {
+    id: "사전",
+    path: "/analysis/dictionary",
+    header: "분석",
+    icon: <Icon.Visibility />,
+    component: Dictionary,
+    children: null
+  },
+  {
+    id: "분석도구",
+    path: "/analysis/tools",
+    icon: <Icon.Visibility />,
+    component: Tools,
+    children: null
+  },
+  {
+    id: "랭킹튜닝",
+    path: "/analysis/ranking-tuning",
+    icon: <Icon.Visibility />,
+    component: RankingTuning,
+    children: null
+  }
+]
 
-const indicesRoutes = {
-  id: "인덱스",
-  path: "/indices",
-  icon: <Icon.Subject />,
-  children: [
-    {
-      path: "/indices/mapping",
-      name: "맵핑",
-      component: Mapping
-    },
-    {
-      path: "/indices/data",
-      name: "데이터",
-      component: IndexData
-    },
-    {
-      path: "/indices/source",
-      name: "소스",
-      component: DataSource
-    },
-    {
-      path: "/indices/index",
-      name: "색인",
-      component: Index
-    },
-    {
-      path: "/indices/configuration",
-      name: "설정",
-      component: Configuration
-    },
-  ]
-}
 
-const searchRoutes = {
-  id: "검색",
-  path: "/search",
-  icon: <Icon.Search />,
-  children: [
-    {
-      path: "/search/query-test",
-      name: "쿼리테스트",
-      component: QueryTest
-    },
-    {
-      path: "/search/reference-ui",
-      name: "레퍼런스UI",
-      component: ReferenceUi
-    }
-  ]
-}
 
-const managementRoutes = {
-  id: "관리",
-  path: "/management",
-  icon: <Icon.Memory />,
-  children: [
-    {
-      path: "/management/server",
-      name: "서버",
-      component: ServerManagement
-    },
-    {
-      path: "/management/user",
-      name: "사용자",
-      component: UserManagement
-    },
-    {
-      path: "/management/api",
-      name: "API",
-      component: ApiManagement
-    },
-    {
-      path: "/management/kibana",
-      name: "Kibana",
-      component: KibanaManagement
-    },
-    {
-      path: "/management/Configuration",
-      name: "설정",
-      component: ConfigurationManagement
-    },
-  ]
-}
+const indicesRoutes = [
+  {
+    header: "인덱스",
+    id: "맵핑",
+    path: "/indices/mapping",
+    icon: <Icon.Subject />,
+    component: Mapping,
+    children: null
+  },
+  {
+    id: "데이터",
+    path: "/indices/data",
+    icon: <Icon.Subject />,
+    component: IndexData,
+    children: null
+  },
+  {
+    id: "소스",
+    path: "/indices/source",
+    icon: <Icon.Subject />,
+    component: DataSource,
+    children: null
+  },
+  {
+    id: "색인",
+    path: "/indices/index",
+    icon: <Icon.Subject />,
+    component: Index,
+    children: null
+  },
+  {
+    id: "설정",
+    path: "/indices/configuration",
+    icon: <Icon.Subject />,
+    component: Configuration,
+    children: null
+  }
+]
+
+const searchRoutes = [
+  {
+    header: "검색",
+    id: "쿼리테스트",
+    path: "/search/query-test",
+    icon: <Icon.Search />,
+    component: QueryTest,
+    children: null
+  },
+  {
+    id: "레퍼런스UI",
+    path: "/search/reference-ui",
+    icon: <Icon.Search />,
+    component: ReferenceUi,
+    children: null
+  },
+]
+
+const managementRoutes = [
+  {
+    header: "관리",
+    id: "서버",
+    path: "/management/server",
+    icon: <Icon.Memory />,
+    component: ServerManagement,
+    children: null
+  },
+  {
+    id: "사용자",
+    path: "/management/user",
+    icon: <Icon.Memory />,
+    component: UserManagement,
+    children: null
+  },
+  {
+    id: "API",
+    path: "/management/api",
+    icon: <Icon.Memory />,
+    component: ApiManagement,
+    children: null
+  },
+  {
+    id: "Kibana",
+    path: "/management/kibana",
+    icon: <Icon.Memory />,
+    component: KibanaManagement,
+    children: null
+  },
+  {
+    id: "설정",
+    path: "/management/configuration",
+    icon: <Icon.Memory />,
+    component: ConfigurationManagement,
+    children: null
+  },
+]
 
 // dashboard layout routing
 export const dashboard = [
   homeRoutes,
   dashboardRoutes,
-  analysisRoutes,
-  indicesRoutes,
-  searchRoutes,
-  managementRoutes
+  ...analysisRoutes,
+  ...indicesRoutes,
+  ...searchRoutes,
+  ...managementRoutes
 ]
 
 // auth layout routing
 export const auth = [authRoutes]
 
 // 메뉴 노출
-export default dashboard
+export default dashboard.filter(o => o !== homeRoutes)
