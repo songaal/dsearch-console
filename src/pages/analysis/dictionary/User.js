940f5fbfc9142e8f74fd670bc560f70ca7ea58f3
jwk <jwk@danawa.com>
2020-06-01 12:27:00 +0900

    데이터 조회, 다운로드 완료, 검색 개발예정

--- src/pages/analysis/dictionary/User.js
+++ src/pages/analysis/dictionary/User.js
@@ -1,35 +1,39 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-// import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import {
     Grid, IconButton, InputBase,
     Button as MuiButton,
     Card, CardContent,
-    Hidden,
+    Box as MuiBox,
+    Checkbox,
 } from "@material-ui/core";
 import DynamicTable from "~/components/DynamicTable";
 import {Language, Search} from "@material-ui/icons";
 import {makeStyles} from "@material-ui/core/styles";
 import {sizing, spacing} from "@material-ui/system";
+import {downloadDictionaryUser, setDictionaryUser} from "../../../redux/actions/dictionaryActions";
+import {setIndicesDataAction} from "../../../redux/actions/indicesIndexDataActions";
 
 const Button = styled(MuiButton)(spacing, sizing)
+const Box = styled(MuiBox)(spacing, sizing)
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
         // margin: theme.spacing(1),
-        minWidth: 250,
+        minWidth: 150,
     },
     form: {
         padding: '2px 4px',
         display: 'flex',
         alignItems: 'center',
-        width: 400,
-        borderBottom: "1px solid"
+        width: 300,
+        // borderBottom: "1px solid"
     },
     input: {
         marginLeft: theme.spacing(1),
         flex: 1,
+        borderBottom: "1px solid"
     },
     iconButton: {
         padding: 5,
@@ -43,38 +47,63 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const sample = [
-    {
-        field: "단어",
-        data: [ {id: 1, text: "1"}, {id: 2, text: "2"}, {id: 3, text: "3"}, {id: 4, text: "4"} ]
-    }
-]
-
-
-
-const dataList1 = [
-    {"field": "단어", data: ['dacco테스트', '테스트dacco', 'fff테스트', 'ff테스트', '테스트fff', '테스트ff', '장혁준667', '장혁준fe', '장혁준ffe', '장혁준ffef']},
-]
-
-const dataList2 = [
-    {"field": "단어", data: ['dacco테스트', '테스트dacco', 'fff테스트', 'ff테스트', '테스트fff', '테스트ff', '장혁준667', '장혁준fe', '장혁준ffe', '장혁준ffef']},
-]
-
-const dataList3 = [
-    {"field": "단어", data: ['dacco테스트', '테스트dacco', 'fff테스트', 'ff테스트', '테스트fff', '테스트ff', '장혁준667', '장혁준fe', '장혁준ffe', '장혁준ffef']},
-]
-
-const dataList4 = [
-    {"field": "단어", data: ['dacco테스트', '테스트dacco', 'fff테스트', 'ff테스트', '테스트fff', '테스트ff', '장혁준667', '장혁준fe', '장혁준ffe', '장혁준ffef']},
-]
-
-function UserDictionary({dispatch, userList, user}) {
-    const [selected, setSelected] = useState([])
+// const sample = [
+//     {
+//         field: "단어",
+//         data: [ {id: 1, text: "1"}, {id: 2, text: "2"}, {id: 3, text: "3"}, {id: 4, text: "4"} ]
+//     }
+// ]
+function UserDictionary({dispatch, user}) {
+    const [selected, setSelected] = useState([]);
+    const [keyword, setKeyword] = useState("");
+    const [keywordMatched, setKeywordMatched] = useState(false);
     const [mode, setMode] = useState("view")  //view, edit
+    const [pageNum, setPageNum] = useState(0);
+    const [rowSize, setRowSize] = useState(40);
     const classes = useStyles();
+
+    let lastPageNum = user['lastPageNum']||0
 
     function handleSelectClick(id, checked) {
         checked ? setSelected(selected.concat(id)) : setSelected(selected.filter(select => select !== id))
+    }
+
+    useEffect(() => {
+        dispatch(setDictionaryUser(pageNum, rowSize))
+    }, [])
+
+
+    function handlePagination(pageNum) {
+        setPageNum(pageNum)
+        dispatch(setDictionaryUser(
+            pageNum,
+            rowSize,
+            keywordMatched ? 'keyword.raw' : 'keyword',
+            keyword
+            )
+        )
+    }
+    function handleInputChange(event) {
+        setPageNum(0)
+        setKeyword(event.target.value)
+        dispatch(setDictionaryUser(
+            0,
+            rowSize,
+            keywordMatched ? 'keyword.raw' : 'keyword',
+            event.target.value
+            )
+        )
+    }
+    function handleCheckboxChange(event) {
+        setPageNum(0)
+        setKeywordMatched(event.target.checked)
+        dispatch(setDictionaryUser(
+            0,
+            rowSize,
+            event.target.checked ? 'keyword.raw' : 'keyword',
+            keyword
+            )
+        )
     }
 
     return (
@@ -84,22 +113,36 @@ function UserDictionary({dispatch, userList, user}) {
                 <CardContent>
                     <Grid container>
                         <Grid item xs={6}>
-                            <form noValidate autoComplete="off" className={classes.form}>
+                            <Box className={classes.form} display={"inline"}>
                                 <InputBase
                                     className={classes.input}
-                                    placeholder="ID"
+                                    placeholder="검색"
+                                    value={keyword}
+                                    onChange={handleInputChange}
                                 />
-                                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                                <IconButton type="submit"
+                                            className={classes.iconButton}
+                                            aria-label="search"
+                                            onClick={event => handlePagination(0)}
+                                >
                                     <Search />
                                 </IconButton>
-                            </form>
+
+                                <Checkbox color="primary"
+                                          value={keywordMatched}
+                                          onChange={handleCheckboxChange}
+                                /> 단어매칭
+
+                            </Box>
                         </Grid>
-                        <Grid item xs={6} className={classes.right}>
+
+                        <Grid item xs={12} md={6} className={classes.right}>
                             {mode === "view" ?
                                 (
                                     <Button variant="outlined"
                                             color="primary"
                                             mx={1}
+                                            onClick={event => downloadDictionaryUser()}
                                     >다운로드</Button>
                                 )
                                 :
@@ -119,6 +162,7 @@ function UserDictionary({dispatch, userList, user}) {
                             <Button variant="outlined"
                                     color="primary"
                                     mx={1}
+                                    onClick={event => handlePagination(pageNum)}
                             >새로고침</Button>
                             <Button variant="outlined"
                                     color="primary"
@@ -128,33 +172,102 @@ function UserDictionary({dispatch, userList, user}) {
                         </Grid>
                     </Grid>
 
+                    {/*<br/>*/}
+
+                    {/*<Grid container>*/}
+                    {/*    <Grid item xs={12}>*/}
+                    {/*        <Box align={"center"}>*/}
+                    {/*            <Button variant={"outlined"}*/}
+                    {/*                    onClick={() => handlePagination(pageNum - 1)}*/}
+                    {/*                    disabled={pageNum === 0}*/}
+                    {/*            >*/}
+                    {/*                이전*/}
+                    {/*            </Button>*/}
+                    {/*            <Box component={"span"} m={3}>*/}
+                    {/*                {lastPageNum === 0 ? 0 : pageNum + 1} / {lastPageNum}*/}
+                    {/*            </Box>*/}
+                    {/*            <Button variant={"outlined"}*/}
+                    {/*                    onClick={() => handlePagination(pageNum + 1)}*/}
+                    {/*                    disabled={(pageNum + 1) === lastPageNum || lastPageNum === 0}*/}
+                    {/*            >*/}
+                    {/*                다음*/}
+                    {/*            </Button>*/}
+                    {/*        </Box>*/}
+                    {/*    </Grid>*/}
+                    {/*</Grid>*/}
+
                     <br/>
                     <Grid container spacing={6}>
                         <Grid item xs={3}>
-                            <DynamicTable dataList={sample}
+                            <DynamicTable dataList={[{
+                                field: "단어",
+                                data: (user.hits||[])
+                                    .filter((hit, index) => index >=  0 && index < 10)
+                                    .map(hit => ({id: hit.id, text: hit.sourceAsMap.keyword}))
+                            }]}
                                           showCheckBox={mode === "edit"}
                                           onSelectClick={handleSelectClick}
                             />
                         </Grid>
                         <Grid item xs={3}>
-                            <DynamicTable dataList={dataList2}
+                            <DynamicTable dataList={[{
+                                field: "단어",
+                                data: (user.hits || [])
+                                    .filter((hit, index) => index >= 10 && index < 20)
+                                    .map(hit => ({id: hit.id, text: hit.sourceAsMap.keyword}))
+                            }]}
                                           showCheckBox={mode === "edit"}
                                           onSelectClick={handleSelectClick}
                             />
                         </Grid>
                         <Grid item xs={3}>
-                            <DynamicTable dataList={dataList3}
+                            <DynamicTable dataList={[{
+                                field: "단어",
+                                data: (user.hits||[])
+                                    .filter((hit, index) => index >= 20 && index < 30)
+                                    .map(hit => ({id: hit.id, text: hit.sourceAsMap.keyword}))
+                            }]}
                                           showCheckBox={mode === "edit"}
                                           onSelectClick={handleSelectClick}
                             />
                         </Grid>
                         <Grid item xs={3}>
-                            <DynamicTable dataList={dataList4}
+                            <DynamicTable dataList={[{
+                                field: "단어",
+                                data: (user.hits||[])
+                                    .filter((hit, index) => index >= 30 && index < 40)
+                                    .map(hit => ({id: hit.id, text: hit.sourceAsMap.keyword}))
+                            }]}
                                           showCheckBox={mode === "edit"}
                                           onSelectClick={handleSelectClick}
                             />
                         </Grid>
                     </Grid>
+
+                    <br/>
+
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Box align={"center"}>
+                                <Button variant={"outlined"}
+                                        onClick={() => handlePagination(pageNum - 1)}
+                                        disabled={pageNum === 0}
+                                >
+                                    이전
+                                </Button>
+                                <Box component={"span"} m={3}>
+                                    {lastPageNum === 0 ? 0 : pageNum + 1} / {lastPageNum}
+                                </Box>
+                                <Button variant={"outlined"}
+                                        onClick={() => handlePagination(pageNum + 1)}
+                                        disabled={(pageNum + 1) === lastPageNum || lastPageNum === 0}
+                                >
+                                    다음
+                                </Button>
+                            </Box>
+                        </Grid>
+                    </Grid>
+
                 </CardContent>
             </Card>
         </>
@@ -162,4 +275,4 @@ function UserDictionary({dispatch, userList, user}) {
     )
 }
 
-export default connect(store => ({userList: store.dictionaryReducers.userList, user: store.dictionaryReducers.user}))(UserDictionary);
+export default connect(store => ({user: store.dictionaryReducers.user}))(UserDictionary);
--- src/redux/actions/dictionaryActions.js
+++ src/redux/actions/dictionaryActions.js
@@ -1,8 +1,26 @@
-import * as types from "../constants";
+import Client from '~/Client'
+import { SET_DICTIONARY_USER } from "../constants";
 
-export function setDictionaryUserList(value) {
-    return {
-        type: types.SET_DICTIONARY_USER_LIST,
-        payload: value
+const client = new Client()
+
+export const setDictionaryUser = (pageNum, rowSize, field, value) => dispatch => client.call({
+    uri: `/dictionaries/user`,
+    params: {
+        pageNum: pageNum,
+        rowSize: rowSize,
+        field: field,
+        value: value
     }
-}
+}).then(response => dispatch({type: SET_DICTIONARY_USER, payload: response.data}))
+    .catch(error => console.error(error))
+
+
+export const downloadDictionaryUser = () => client.call({uri: `/dictionaries/user/download`,  responseType: 'blob'})
+    .then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data], { type: response.headers['content-type'] }));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'user.txt');
+        document.body.appendChild(link);
+        link.click();
+    })
