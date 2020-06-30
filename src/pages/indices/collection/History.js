2ffc7d2fa33fb339a8f02a1200a4886884fcb580
joonwoo8888 <dev.joonwoo@gmail.com>
2020-06-30 18:08:25 +0900

    .

--- src/pages/indices/collection/History.js
+++ src/pages/indices/collection/History.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import Helmet from 'react-helmet';
 
@@ -7,13 +7,27 @@ import {
     Button as MuiButton,
     Card as MuiCard,
     Divider as MuiDivider,
+    Fade,
     Grid as MuiGrid,
+    Link,
+    Menu,
+    MenuItem,
+    Paper,
+    Popper,
+    Select,
+    Switch,
+    Table,
+    TableBody,
+    TableCell, TableHead,
+    TableRow,
+    TextField,
     Typography as MuiTypography,
 } from "@material-ui/core";
 import {makeStyles} from '@material-ui/core/styles';
 import {positions, spacing} from "@material-ui/system";
 import AntTabs from "~/components/AntTabs"
 import SearchIcon from "@material-ui/icons/Search";
+import {ArrowDropDown} from "@material-ui/icons";
 
 const Divider = styled(MuiDivider)(spacing, positions);
 const Typography = styled(MuiTypography)(spacing, positions);
@@ -38,10 +52,126 @@ const useStyles = makeStyles((theme) => ({
 
 function History() {
     const classes = useStyles();
+    const [moreMenu, setMoreMenu] = useState(null)
+
+    function toggleMoreMenu(event) {
+        setMoreMenu(moreMenu === null ? event.currentTarget : null)
+    }
 
     return (
         <React.Fragment>
-            히스토리
+            <br/>
+
+            <Box>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>#</TableCell>
+                            <TableCell>타입</TableCell>
+                            <TableCell>결과</TableCell>
+                            <TableCell>문서수</TableCell>
+                            <TableCell>자동실행</TableCell>
+                            <TableCell>시작</TableCell>
+                            <TableCell>종료</TableCell>
+                            <TableCell>소요시간</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>5</TableCell>
+                            <TableCell>전파</TableCell>
+                            <TableCell>성공</TableCell>
+                            <TableCell>45,233</TableCell>
+                            <TableCell>수동</TableCell>
+                            <TableCell>2020-06-22 14:00:40</TableCell>
+                            <TableCell>2020-06-22 15:00:40</TableCell>
+                            <TableCell>1h</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>4</TableCell>
+                            <TableCell>색인</TableCell>
+                            <TableCell>성공</TableCell>
+                            <TableCell>45,233</TableCell>
+                            <TableCell>수동</TableCell>
+                            <TableCell>2020-06-22 14:00:40</TableCell>
+                            <TableCell>2020-06-22 15:00:40</TableCell>
+                            <TableCell>1h</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>3</TableCell>
+                            <TableCell>색인</TableCell>
+                            <TableCell>성공</TableCell>
+                            <TableCell>45,233</TableCell>
+                            <TableCell>수동</TableCell>
+                            <TableCell>2020-06-22 14:00:40</TableCell>
+                            <TableCell>2020-06-22 15:00:40</TableCell>
+                            <TableCell>1h</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>2</TableCell>
+                            <TableCell>전파</TableCell>
+                            <TableCell>성공</TableCell>
+                            <TableCell>45,233</TableCell>
+                            <TableCell>수동</TableCell>
+                            <TableCell>2020-06-22 14:00:40</TableCell>
+                            <TableCell>2020-06-22 15:00:40</TableCell>
+                            <TableCell>1h</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>1</TableCell>
+                            <TableCell>색인</TableCell>
+                            <TableCell>성공</TableCell>
+                            <TableCell>45,233</TableCell>
+                            <TableCell>수동</TableCell>
+                            <TableCell>2020-06-22 14:00:40</TableCell>
+                            <TableCell>2020-06-22 15:00:40</TableCell>
+                            <TableCell>1h</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </Box>
+
+            <br/>
+            <Grid container>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={8}>
+                    <Box align={"center"}>
+                        <Box align={"center"}>
+                            <Button variant={"outlined"}>
+                                이전
+                            </Button>
+                            <Box component={"span"} m={3}>
+                                1 / 3
+                            </Box>
+                            <Button variant={"outlined"}>
+                                다음
+                            </Button>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={2}>
+                    <Box align={'right'}>
+                        <Button variant={"outlined"}
+                                onClick={toggleMoreMenu}
+                        >
+                            더보기
+                            <ArrowDropDown/>
+                        </Button>
+                        <Menu
+                            anchorEl={moreMenu}
+                            open={Boolean(moreMenu)}
+                            onClose={toggleMoreMenu}
+                        >
+                            <MenuItem>
+                                초가화
+                            </MenuItem>
+                            <MenuItem>
+                                7일이전 모두 삭제
+                            </MenuItem>
+                        </Menu>
+                    </Box>
+                </Grid>
+            </Grid>
 
         </React.Fragment>
     );
--- src/pages/indices/collection/Source.js
+++ src/pages/indices/collection/Source.js
@@ -5,9 +5,26 @@ import Helmet from 'react-helmet';
 import {
     Box as MuiBox,
     Button as MuiButton,
-    Card as MuiCard, Dialog, DialogActions, DialogContent, DialogTitle,
+    Card as MuiCard,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
     Divider as MuiDivider,
-    Grid as MuiGrid, Link, Menu, MenuItem, Switch, Table, TableBody, TableCell, TableRow, TextField,
+    Fade,
+    Grid as MuiGrid,
+    Link,
+    Menu,
+    MenuItem,
+    Paper,
+    Popper,
+    Select,
+    Switch,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow,
+    TextField,
     Typography as MuiTypography,
 } from "@material-ui/core";
 import {makeStyles} from '@material-ui/core/styles';
@@ -33,7 +50,10 @@ const useStyles = makeStyles((theme) => ({
     },
     edit: {
         width: '100%'
-    }
+    },
+    typography: {
+        padding: theme.spacing(2),
+    },
 }));
 
 
@@ -41,6 +61,10 @@ function Source() {
     const classes = useStyles();
     const [moreMenu, setMoreMenu] = useState(null)
     const [editModal, setEditModal] = useState(null)
+    const [mode, setMode] = useState("VIEW")
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [open, setOpen] = React.useState(false);
+    const [placement, setPlacement] = React.useState();
 
     function toggleMoreMenu(event) {
         setMoreMenu(moreMenu === null ? event.currentTarget : null)
@@ -48,95 +72,181 @@ function Source() {
     function toggleEditModal(event) {
         setEditModal(editModal === null ? event.currentTarget : null)
     }
-
+    const handleClick = (newPlacement) => (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpen((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
+    }
     return (
         <React.Fragment>
 
             <br/>
 
-            <Grid container>
-                <Grid item xs={10}>
-                    <Grid container my={3}>
-                        <Grid item xs={2} mt={2}>
-                            스케쥴 사용여부
+            <Box style={{display: mode === "VIEW" ? "block" : "none"}}>
+                <Grid container>
+                    <Grid item xs={10}>
+                        <Grid container my={3}>
+                            <Grid item xs={2} mt={2}>
+                                스케쥴 사용여부
+                            </Grid>
+                            <Grid item xs={10}>
+                                <Switch />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={10}>
-                            <Switch />
+
+                        <Grid container my={3}>
+                            <Grid item xs={1} mt={2}>
+                                상태
+                            </Grid>
+                            <Grid item xs={1} mt={2}>
+                                대기
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Button variant={"outlined"}
+                                        onClick={toggleMoreMenu}
+                                >
+                                    더보기
+                                    <ArrowDropDown/>
+                                </Button>
+                                <Menu
+                                    anchorEl={moreMenu}
+                                    open={Boolean(moreMenu)}
+                                    onClose={toggleMoreMenu}
+                                >
+                                    <MenuItem onClick={toggleMoreMenu}>
+                                        연속실행
+                                    </MenuItem>
+                                    <MenuItem onClick={toggleMoreMenu}>
+                                        1.색인실행
+                                    </MenuItem>
+                                    <MenuItem onClick={toggleMoreMenu}>
+                                        2.전파실행
+                                    </MenuItem>
+                                    <MenuItem onClick={toggleMoreMenu}>
+                                        3.교체실행
+                                    </MenuItem>
+                                </Menu>
+                            </Grid>
                         </Grid>
                     </Grid>
-
-                    <Grid container my={3}>
-                        <Grid item xs={1} mt={2}>
-                            상태
-                        </Grid>
-                        <Grid item xs={1} mt={2}>
-                            대기
-                        </Grid>
-                        <Grid item xs={8}>
-                            <Button variant={"outlined"}
-                                    onClick={toggleMoreMenu}
-                            >
-                                더보기
-                                <ArrowDropDown/>
-                            </Button>
-                            <Menu
-                                anchorEl={moreMenu}
-                                open={Boolean(moreMenu)}
-                                onClose={toggleMoreMenu}
-                            >
-                                <MenuItem onClick={toggleMoreMenu}>
-                                    연속실행
-                                </MenuItem>
-                                <MenuItem onClick={toggleMoreMenu}>
-                                    1.색인실행
-                                </MenuItem>
-                                <MenuItem onClick={toggleMoreMenu}>
-                                    2.전파실행
-                                </MenuItem>
-                                <MenuItem onClick={toggleMoreMenu}>
-                                    3.교체실행
-                                </MenuItem>
-                            </Menu>
-                        </Grid>
+                    <Grid item xs={2} align={"right"}>
+                        <Button variant={"outlined"} onClick={() => setMode("EDIT")}>
+                            수정
+                        </Button>
                     </Grid>
                 </Grid>
-                <Grid item xs={2} align={"right"}>
-                    <Button variant={"outlined"} >
-                        수정
-                    </Button>
-                </Grid>
-            </Grid>
 
-            <Grid container>
-                <Grid item xs={12}>
-                    <Box p={5}>
-                        <Table>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell>런처</TableCell>
-                                    <TableCell>dbIndexer.jar
-                                        <Link style={{cursor: "pointer"}}
-                                              onClick={toggleEditModal}
-                                        >수집설정</Link>
-                                    </TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>실행포트</TableCell>
-                                    <TableCell>30100</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>JDBC</TableCell>
-                                    <TableCell>market-dev</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>크론주기</TableCell>
-                                    <TableCell>0분 5시 *일 *월 *요일</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </Box>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box>
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>런처</TableCell>
+                                        <TableCell>dbIndexer.jar
+                                            <Link style={{cursor: "pointer"}}
+                                                  onClick={toggleEditModal}
+                                            >수집설정</Link>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>실행포트</TableCell>
+                                        <TableCell>30100</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>JDBC</TableCell>
+                                        <TableCell>market-dev</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>크론주기</TableCell>
+                                        <TableCell>0분 5시 *일 *월 *요일</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Box>
+            <Box style={{display: mode === "EDIT" ? "block" : "none"}}>
+
+                <Grid container>
+                    <Grid item xs={10}>
+
+                    </Grid>
+                    <Grid item xs={2} align={"right"}>
+                        <Button variant={"outlined"} onClick={() => setMode("VIEW")}>
+                            저장
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box p={5}>
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>이름</TableCell>
+                                        <TableCell>
+                                            <TextField value={"기준상품 전체색인 수집"} fullWidth/>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>런처</TableCell>
+                                        <TableCell>
+                                            <TextField value={"dbIndexer.jar"} fullWidth/>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>실행포트</TableCell>
+                                        <TableCell>
+                                            <TextField value={"30100"} fullWidth/>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>JDBC</TableCell>
+                                        <TableCell>
+                                            <Select value={"market-dev"}>
+                                                <MenuItem value={"market-dev"}>market-dev</MenuItem>
+                                                <MenuItem value={"market-dev1"}>market-dev1</MenuItem>
+                                                <MenuItem value={"market-dev2"}>market-dev2</MenuItem>
+                                            </Select>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>크론주기</TableCell>
+                                        <TableCell>
+                                            <Grid container>
+                                                <Grid item xs={11}>
+                                                    <TextField value={"30 0 2 * * *"} fullWidth placeholder={"분 시 일 월 요일"}/>
+                                                </Grid>
+                                                <Grid item xs={1}>
+                                                    <Link onMouseOver={handleClick('top')}>예제</Link>
+                                                    <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+                                                        {({ TransitionProps }) => (
+                                                            <Fade {...TransitionProps} timeout={350}>
+                                                                <Paper>
+                                                                    <Typography className={classes.typography}>
+                                                                        예제<br/>
+                                                                        */5 * * * * : 5분마다 한 번씩<br/>
+                                                                        */5 * * * * : 5분마다 한 번씩<br/>
+                                                                        0 5 1 * * : 매달 1일 새벽 5시에 실행.<br/>
+                                                                        0 5,11 * * 0,3 : 매주 일요일과 수요일 새벽 5시와 밤 11시.<br/>
+                                                                        0 5,11 * * * : 새벽 5시와 밤 11시
+                                                                    </Typography>
+                                                                </Paper>
+                                                            </Fade>
+                                                        )}
+                                                    </Popper>
+                                                </Grid>
+                                            </Grid>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
 
 
             <Dialog open={editModal}
