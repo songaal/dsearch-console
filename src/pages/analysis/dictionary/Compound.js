3c15fe0a5e0907df6d87822ab1b01a895a8d8f6d
sunjiho <sunjiho@gitlab.com>
2021-09-07 18:35:31 +0900

    trim 적용

--- src/pages/analysis/dictionary/Compound.js
+++ src/pages/analysis/dictionary/Compound.js
@@ -165,9 +165,11 @@ function CompoundDictionary({dictionary, authUser, setting, dataSet, totalCount}
         }
         if(newCreateKeyword.current != null){
             createKeyword = newCreateKeyword.current.value
+            createKeyword = createKeyword.trim()
         }
         if(newCreateValue.current != null){
             createValue = newCreateValue.current.value
+            createValue = createValue.trim()
         }
         let data = {id: createId, keyword: createKeyword, value: createValue}
         await createDictionary(dictionary, JSON.stringify(data))
